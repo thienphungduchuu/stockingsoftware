c3eb71e6abb2610bcf3b13a40c9e6e3699f57991
thienphungduchuu <thienphungduchuu_december@yahoo.com>
2022-01-30 20:38:28 -0700

    Initialize the Project

---
+++ src/main/java/com/tap/stockingsoftware/StockingsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.tap.stockingsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockingsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockingsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tap/stockingsoftware/StockingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap.stockingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
